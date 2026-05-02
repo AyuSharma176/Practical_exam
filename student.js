9bd7a6f89631e61bb8879fac4925cbdfb1badad2
AyuSharma176 <ayusharma1706@gmail.com>
2026-05-02 15:44:53 +0530

    ok

--- student.js
+++ student.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const studentSchema = new mongoose.Schema(
   {
-    id: { type: Number, unique: true },
     name: { type: String },
     marks: { type: Number },
     course: { type: String },
